db2ce88e750c028966c9f256f5c13f0f050e7cb1
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-10-18 07:10:13 -0500

    remove class list

---
+++ script.js
@@ -0,0 +1,17 @@
+const contents = document.querySelectorAll('.content')
+const listItems = document.querySelectorAll('nav ul li')
+
+listItems.forEach((item, idx) => {
+  item.addEventListener('click', () => {
+    hideAllContents()
+    hideAllItems()
+  })
+})
+
+function hideAllContents() {
+  contents.forEach(content => content.classList.remove('show'))
+}
+
+function hideAllItems() {
+  listItems.forEach(item => item.classList.remove('active'))
+}
